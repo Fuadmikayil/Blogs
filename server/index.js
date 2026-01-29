784a05335d6ea658a937b5ac8433dd1e4d826465
Fuadmikayil <mikayilov2006@gmail.com>
2026-01-29 21:42:16 +0400

    vercel link

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT;
 const MONGODB_URI = process.env.MONGODB_URI;
 
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://blogs-two-topaz.vercel.app/'],
+  origin: ['http://localhost:3000', 'https://blogs-fawn-rho.vercel.app/'],
   credentials: true,
 }));
 
